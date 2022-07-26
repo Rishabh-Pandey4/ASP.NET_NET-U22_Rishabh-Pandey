fc0fc97a2e55e4f856bd01978e7f0c6414070598
Rishabh-Pandey4 <110027548+Rishabh-Pandey4@users.noreply.github.com>
2022-07-26 16:41:38 +0530

    Tasks for the ASP.NET Training
    Added Task1 for Day 1 and Day 2 of ASP.NET Training

---
+++ Codes/Day2/Task1/Models/EmployeeManager.cs
@@ -0,0 +1,45 @@
+﻿namespace Task1.Models
+{
+    public class EmployeeManager
+    {
+        public static List<Emp> Employees = new List<Emp>();
+
+        public EmployeeManager()
+        {
+            Employees = new List<Emp>()
+            {
+                new Emp(){Empid = 1, Ename = "Tim", Job = "Manager", Salary= 10000, Deptno = 30},
+                new Emp(){Empid = 2, Ename = "Jim", Job = "Sales", Salary= 8000, Deptno = 20},
+                new Emp(){Empid = 3, Ename = "Tom", Job = "IT", Salary= 9000, Deptno = 10}
+            };
+        }
+
+        public List<Emp> GetAllEmp()
+        {
+            return Employees;
+        }
+        public Emp GetEmpById(int id)
+        {
+            return Employees.Find(item => item.Empid == id);
+        }
+        public void AddEmp(Emp obj)
+        {
+            Employees.Add(obj);
+        }
+        public void DeleteEmp(int id)
+        {
+            Emp obj = Employees.Find(item => item.Empid == id);
+            Employees.Remove(obj);
+        }
+        public void UpdateEmp(Emp updated_Obj)
+        {
+            Emp obj = Employees.Find(item => item.Empid == updated_Obj.Empid);
+            obj.Ename = updated_Obj.Ename;
+            obj.Salary = updated_Obj.Salary;
+            obj.Job = updated_Obj.Job;
+            obj.Deptno = updated_Obj.Deptno;
+        }
+
+
+    }
+}
