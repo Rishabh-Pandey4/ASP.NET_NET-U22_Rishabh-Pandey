28b598062d89cf9cf73884bd3102c51829144a8a
Rishabh-Pandey4 <110027548+Rishabh-Pandey4@users.noreply.github.com>
2022-07-29 15:05:29 +0530

    Add files via upload

---
+++ Codes/Day5/Task1/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Task1.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string Ename { get; set; }
+        public string Job { get; set; }
+
+        public int Salary { get; set; }
+
+        public int Deptno { get; set; }
+    }
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+
+        }
+    }
+
+}
